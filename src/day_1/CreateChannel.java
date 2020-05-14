c5f0fdc3de0df25424b99cf54a8be25cdb989eda
Ceitze <lusiferc679@gmail.com>
2020-05-14 11:28:22 +0300

    update CretaeChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,14 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("User is able to create channel");
+        System.out.println(         );
+        System.out.println("what is conflit");
+
+
+        for (String arg : args) {
+
+            break;
+        }
+        }
     }
-}
+
