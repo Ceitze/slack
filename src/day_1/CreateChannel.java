83c90b43dcd084a10cd86303e6ea644296acf4b7
Ahmet <59234976+Ceitze@users.noreply.github.com>
2020-05-14 11:31:58 +0300

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,20 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("User is able to create channel");
+   
+    
+     @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+
+    }
+
+    @AfterMethod
+    public void afterMethod() throws InterruptedException {
+        Thread.sleep(4000);
+        driver.quit();
+    
+    
+    
     }
 }
