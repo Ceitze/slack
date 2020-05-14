5bf010d8c055c2e7198a8a42765f52f33012f4ce
Ceitze <lusiferc679@gmail.com>
2020-05-14 11:42:20 +0300

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,31 +5,22 @@ public class CreateChannel {
 
         System.out.println("User is able to create channel");
 
-        System.out.println(         );
+        System.out.println();
         System.out.println("what is conflit");
 
 
-        for (String arg : args) {//changed from inteliJ
+        //changed from inteliJ
+        //
+        //
+        //
+        //
+//
+        //
+        //
+        //
+        //
+        //
 
-            break;
-        }
-        }
-
-   
-    
-     @BeforeMethod
-    public void setUpMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-
-    }
-
-    @AfterMethod
-    public void afterMethod() throws InterruptedException {
-        Thread.sleep(4000);
-        driver.quit();
-    
-    
-    
-
-    }
-
+        //
+//
+    }}
